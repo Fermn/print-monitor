081aead8cc3707a2acf0dd63539d646a0a621e51
Your Name <your-email@example.com>
2024-12-23 10:52:56 -0800

    add robocop gem & add index.erb to root

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source "https://rubygems.org"
 
 # gem "rails"
+gem 'rubocop', '~> 1.69', '>= 1.69.2'
 gem 'sinatra', '~> 4.1', '>= 4.1.1'
 gem 'snmp', '~> 1.3', '>= 1.3.2'
 gem 'json', '~> 2.9', '>= 2.9.1'
--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'snmp' 
+require 'snmp'
 require 'json'
 
 printer_config = JSON.parse(File.read('printers.json'))
@@ -7,31 +7,35 @@ COMMON_OIDS = printer_config['common_oids']
 PRINTERS = printer_config['printers']
 
 def fetch_printer_data(printer_ip, common_oids, specific_oids)
-    data = {}
-    oids = common_oids.merge(specific_oids)
-    SNMP::Manager.open(host: printer_ip) do |manager|
-        oids.each do |key, oid|
-            response = manager.get(oid)
-            data[key] = response.varbind_list.first.value.to_s
-        end
+  data = {}
+  oids = common_oids.merge(specific_oids)
+  SNMP::Manager.open(host: printer_ip) do |manager|
+    oids.each do |key, oid|
+      response = manager.get(oid)
+      data[key] = response.varbind_list.first.value.to_s
     end
-    data
+  end
+  data
 end
 
 get '/' do
-    "Hello from the index page!"
+  erb :index
+end
+
+get '/printers/new' do
+  erb :new_printer
 end
 
 get '/printers' do
-    @fetch_printers_data  = PRINTERS.map do |printer|
-      data = fetch_printer_data(printer['printer_ip'], COMMON_OIDS, printer['specific_oids'])
-      {
-        alias: printer['alias'],
-        ip: printer['printer_ip'],
-        model: data['model'],
-        data: data
-      }
-    end  
-    puts "Fetched printer data: #{@fetch_printer_data.inspect}"
-    erb :index
+  @fetch_printers_data = PRINTERS.map do |printer|
+    data = fetch_printer_data(printer['printer_ip'], COMMON_OIDS, printer['specific_oids'])
+    {
+      alias: printer['alias'],
+      ip: printer['printer_ip'],
+      model: data['model'],
+      data: data
+    }
+  end
+  puts "Fetched printer data: #{@fetch_printer_data.inspect}"
+  erb :index
 end
