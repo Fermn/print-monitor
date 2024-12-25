6218a3a595e704732a57bcefe0ab9a3e0e5a090a
Fermin <46830086+Fermn@users.noreply.github.com>
2024-12-24 23:34:09 -0800

    restructure root route

--- app.rb
+++ app.rb
@@ -8,37 +8,19 @@ PRINTERS = printer_config['printers']
 
 def fetch_printer_data(printer_ip, common_oids, specific_oids)
   data = {}
-  oids = common_oids.merge(specific_oids)
   SNMP::Manager.open(host: printer_ip) do |manager|
-    oids.each do |key, oid|
+    common_oids.merge(specific_oids).each do |key, oid|
       response = manager.get(oid)
       data[key] = response.varbind_list.first.value.to_s
+    rescue StandardError => e
+      puts "Error fetching OID #{oid} for printer #{printer_ip}: #{e.message}"
+      data[key] = 'Error'
     end
   end
   data
 end
 
 get '/' do
-  erb :index
-end
-
-get '/printers/new' do
-  erb :new_printer
-end
-
-post '/printers' do
-  printer = {
-    'alias' => params[:alias],
-    'printer_ip' => params[:printer_ip]
-  }
-  PRINTERS << printer
-  File.open('printers.json', 'w') do |f|
-    f.write(JSON.pretty_generate(PRINTERS))
-  end
-  redirect '/printers'
-end
-
-get '/printers' do
   @fetch_printers_data = PRINTERS.map do |printer|
     data = fetch_printer_data(printer['printer_ip'], COMMON_OIDS, printer['specific_oids'])
     {
@@ -50,4 +32,20 @@ get '/printers' do
   end
   puts "Fetched printer data: #{@fetch_printer_data.inspect}"
   erb :index
+end
+
+get '/new' do
+  erb :new_printer
+end
+
+post '/' do
+  printer = {
+    'alias' => params[:alias],
+    'printer_ip' => params[:printer_ip]
+  }
+  PRINTERS << printer
+  File.open('printers.json', 'w') do |f|
+    f.write(JSON.pretty_generate(PRINTERS))
+  end
+  redirect '/'
 end
