78360a913741f8a038e8e77c94e6c8a57f4612c8
Fermin <46830086+Fermn@users.noreply.github.com>
2024-12-25 00:34:16 -0800

    restructure files to use mock printer data for frontend dev

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'snmp'
 require 'json'
+require_relative 'mock_data' if ENV['RACK_ENV'] == 'development'
 
 printer_config = JSON.parse(File.read('printers.json'))
 COMMON_OIDS = printer_config['common_oids']
@@ -8,29 +9,39 @@ PRINTERS = printer_config['printers']
 
 def fetch_printer_data(printer_ip, common_oids, specific_oids)
   data = {}
-  SNMP::Manager.open(host: printer_ip) do |manager|
-    common_oids.merge(specific_oids).each do |key, oid|
-      response = manager.get(oid)
-      data[key] = response.varbind_list.first.value.to_s
-    rescue StandardError => e
-      puts "Error fetching OID #{oid} for printer #{printer_ip}: #{e.message}"
-      data[key] = 'Error'
+  if ENV['RACK_ENV'] == 'development'
+    MOCK_PRINTERS.each do |printer|
+      return printer['data'] if printer['printer_ip'] == printer_ip
+    end
+  else
+    SNMP::Manager.open(host: printer_ip) do |manager|
+      common_oids.merge(specific_oids).each do |key, oid|
+        response = manager.get(oid)
+        data[key] = response.varbind_list.first.value.to_s
+      rescue StandardError => e
+        puts "Error fetching OID #{oid} for printer #{printer_ip}: #{e.message}"
+        data[key] = 'Error'
+      end
     end
   end
   data
 end
 
 get '/' do
-  @fetch_printers_data = PRINTERS.map do |printer|
-    data = fetch_printer_data(printer['printer_ip'], COMMON_OIDS, printer['specific_oids'])
-    {
-      alias: printer['alias'],
-      ip: printer['printer_ip'],
-      model: data['model'],
-      data: data
-    }
-  end
-  puts "Fetched printer data: #{@fetch_printer_data.inspect}"
+  @fetch_printers_data = if ENV['RACK_ENV'] == 'development'
+                           MOCK_PRINTERS
+                         else
+                           PRINTERS.map do |printer|
+                             data = fetch_printer_data(printer['printer_ip'], COMMON_OIDS, printer['specific_oids'])
+                             {
+                               alias: printer['alias'],
+                               ip: printer['printer_ip'],
+                               model: data['model'],
+                               data: data
+                             }
+                           end
+                         end
+  puts "Fetched printer data: #{@fetch_printers_data.inspect}"
   erb :index
 end
 
