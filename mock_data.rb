0fdb442517146cc4978840dbb7b8969dcb7662c5
Your Name <your-email@example.com>
2025-01-07 11:17:10 -0800

    restructure mock data to include page impressions & ink cartridge levels

--- mock_data.rb
+++ mock_data.rb
@@ -1,82 +1,146 @@
 MOCK_PRINTERS = [
   {
+    printer_ip: '192.168.1.109',
+    status: 'Online',
     alias: 'Printer 1',
-    printer_ip: '192.168.1.101',
-    model: 'Model A',
-    data: {
-      status: 'Online',
+    model: 'Model H',
+    impressions: {
+      total: '8561',
+      bw: '654',
+      color: '421'
+    },
+    toner: {
       pages_printed: '1234',
-      toner_level: '75%'
+      black_level: '34%',
+      yellow_level: '64%',
+      cyan_level: '12%',
+      magenta_level: '87%'
     }
   },
   {
+    printer_ip: '192.168.1.108',
+    status: 'Offline',
     alias: 'Printer 2',
-    printer_ip: '192.168.1.102',
-    model: 'Model B',
-    data: {
-      status: 'Offline',
+    model: 'Model V',
+    impressions: {
+      total: '5426',
+      bw: '423',
+      color: '754'
+    },
+    toner: {
       pages_printed: '5678',
-      toner_level: '50%'
+      black_level: '45%',
+      yellow_level: '76%',
+      cyan_level: '23%',
+      magenta_level: '87%'
     }
   },
   {
+    printer_ip: '192.168.1.107',
+    status: 'Online',
     alias: 'Printer 3',
-    printer_ip: '192.168.1.101',
     model: 'Model A',
-    data: {
-      status: 'Online',
+    impressions: {
+      total: '7845',
+      bw: '654',
+      color: '215'
+    },
+    toner: {
       pages_printed: '1234',
-      toner_level: '75%'
+      black_level: '43%',
+      yellow_level: '76%',
+      cyan_level: '23%',
+      magenta_level: '44%'
     }
   },
   {
+    printer_ip: '192.168.1.106',
+    status: 'Offline',
     alias: 'Printer 4',
-    printer_ip: '192.168.1.102',
-    model: 'Model B',
-    data: {
-      status: 'Offline',
+    model: 'Model C',
+    impressions: {
+      total: '9654',
+      bw: '564',
+      color: '321'
+    },
+    toner: {
       pages_printed: '5678',
-      toner_level: '50%'
+      black_level: '34%',
+      yellow_level: '22%',
+      cyan_level: '76%',
+      magenta_level: '56%'
     }
   },
   {
+    printer_ip: '192.168.1.104',
+    status: 'Online',
     alias: 'Printer 5',
-    printer_ip: '192.168.1.101',
-    model: 'Model A',
-    data: {
-      status: 'Online',
+    model: 'Model F',
+    impressions: {
+      total: '6521',
+      bw: '523',
+      color: '631'
+    },
+    toner: {
       pages_printed: '1234',
-      toner_level: '75%'
+      black_level: '43%',
+      yellow_level: '76%',
+      cyan_level: '65%',
+      magenta_level: '33%'
     }
   },
   {
+    printer_ip: '192.168.1.103',
+    status: 'Offline',
     alias: 'Printer 6',
-    printer_ip: '192.168.1.102',
-    model: 'Model B',
-    data: {
-      status: 'Offline',
+    model: 'Model T',
+    impressions: {
+      total: '9542',
+      bw: '455',
+      color: '635'
+    },
+    toner: {
       pages_printed: '5678',
-      toner_level: '50%'
+      black_level: '43%',
+      yellow_level: '54%',
+      cyan_level: '45%',
+      magenta_level: '54%'
     }
   },
   {
-    alias: 'Printer 7',
     printer_ip: '192.168.1.101',
+    status: 'Online',
+    alias: 'Printer 7',
     model: 'Model A',
-    data: {
-      status: 'Online',
+    impressions: {
+      total: '6541',
+      bw: '325',
+      color: '635'
+    },
+    toner: {
       pages_printed: '1234',
-      toner_level: '75%'
+      black_level: '55%',
+      yellow_level: '43%',
+      cyan_level: '43%',
+      magenta_level: '22%'
     }
   },
   {
-    alias: 'Printer 8',
     printer_ip: '192.168.1.102',
+    status: 'Offline',
+    alias: 'Printer 8',
     model: 'Model B',
-    data: {
-      status: 'Offline',
+    impressions: {
+      total: '3212',
+      bw: '878',
+      color: '635'
+    },
+    toner: {
       pages_printed: '5678',
-      toner_level: '50%'
+      black_level: '50%',
+      yellow_level: '25%',
+      cyan_level: '85%',
+      magenta_level: '60%'
     }
   }
 ]
